60c5c687d8fd3bddafebb39016b6fc071576bdbf
blacky0x0 <blacky0x0@users.noreply.github.com>
2015-02-17 02:43:15 +0300

    Add a GUI converter (javafx) to compute 32bit direct, reverse, additional codes

---
+++ numeral-systems/src/com/github/blacky0x0/IntegerUtil.java
@@ -0,0 +1,126 @@
+package com.github.blacky0x0;
+
+import java.util.Arrays;
+
+/**
+ * User: blacky
+ * Date: 16.02.15
+ */
+public class IntegerUtil {
+
+    /**
+     * Direct code for 32 bits. For absolute value.
+     * @param num decimal value
+     * @return direct binary code for 32 bits
+     */
+    public static String getDirectCodeForAbsoluteValue(int num) {
+        char[] leadingChars = new char[Integer.numberOfLeadingZeros(Math.abs(num))];
+        String result = Integer.toBinaryString(Math.abs(num));
+        Arrays.fill(leadingChars, '0');
+
+        if (num == 0)
+            return String.valueOf(leadingChars);
+
+        return String.valueOf(leadingChars).concat(result);
+    }
+
+    /**
+     * Direct code for 32 bits. Exclude negative zero (-0) value.
+     *
+     * <p>For example:
+     * <ul>
+     *     <li>-1 -> 10000000000000000000000000000001</li>
+     *     <li>&nbsp;1 -> 00000000000000000000000000000001</li>
+     * </p>
+     * @param num decimal value
+     * @return direct binary code for 32 bits
+     */
+    public static String getDirectCode(int num) {
+        String result = getDirectCodeForAbsoluteValue(num);
+
+        if (sign(num) == -1)
+            result = "1".concat(result.substring(1));
+
+        return result;
+    }
+
+    /**
+     * Inverts ones to zeros & the other way around.
+     <p>For example:
+     * <ul>
+     *     <li>1101 -> 0010</li>
+     * </p>
+     * @param code contains of ones & zeros
+     * @return inverted values in string
+     */
+    public static String invertBits(String code) {
+        StringBuilder sb = new StringBuilder(code.length());
+        for (char ch : code.toCharArray()) {
+            if (ch == '1')
+                sb.append('0');
+            else
+                sb.append('1');
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Reverse code for 32 bits. Exclude negative zero (-0) value.
+     * @param num decimal value
+     * @return reverse binary code for 32 bits
+     */
+    public static String getReverseCode(int num) {
+        if (sign(num) == -1)
+            return invertBits(getDirectCodeForAbsoluteValue(num));
+
+        return getDirectCodeForAbsoluteValue(num);
+    }
+
+    /**
+     * Additional code for 32 bits. Exclude negative zero (-0) value.
+     * @param num decimal value
+     * @return additional binary code for 32 bits
+     */
+    public static String getAdditionalCode(int num) {
+        char[] leadingChars = new char[Integer.numberOfLeadingZeros(num)];
+        String result = Integer.toBinaryString(num);
+        Arrays.fill(leadingChars, '0');
+
+        if (num == 0)
+            return String.valueOf(leadingChars);
+
+        return String.valueOf(leadingChars).concat(result);
+    }
+
+    public static int signBit(float f) {
+        return Float.floatToIntBits(f)>>>31;
+    }
+
+    public static int sign(int i) {
+        if (i == 0) return 0;
+        if (i >> 31 == 0) return +1;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println((getDirectCode(-32)));
+//        System.out.println((getReverseCode(-32)));
+//        System.out.println((getAdditionalCode(-32)));
+    }
+
+    public static String getFormattedCode(String code) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < code.length() / 4; i++) {
+            sb.append(code.substring(i * 4, i * 4 + 4));
+            sb.append(' ');
+        }
+
+        return sb.toString();
+    }
+
+    public static String getRuller() {
+        return "7654 3210 7654 3210 7654 3210 7654 3210";
+    }
+}
---
+++ numeral-systems/src/com/github/blacky0x0/gui/IntegerUtilGuiConverter.java
@@ -0,0 +1,110 @@
+package com.github.blacky0x0.gui;
+
+import com.github.blacky0x0.IntegerUtil;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * User: blacky
+ * Date: 17.02.15
+ */
+public class IntegerUtilGuiConverter extends Application {
+
+    public Parent createContent() {
+        //Controls to be added to the HBox
+        Label decimalLabel = new Label("Decimal number:");
+        TextField decimalField = new TextField();
+        Button decimalButton = new Button("OK");
+
+        Label directCodeLabel = new Label("Direct code:");
+        TextField directCodeField = new TextField();
+
+        Label reverseCodeLabel = new Label("Reverse code:");
+        TextField reverseCodeField = new TextField();
+
+        Label additionalCodeLabel = new Label("Additional code:");
+        TextField additionalCodeField = new TextField();
+
+        Button clearButton = new Button("Clear");
+
+        decimalField.setPromptText("42");
+        directCodeField.setEditable(false);
+        reverseCodeField.setEditable(false);
+        additionalCodeField.setEditable(false);
+
+        decimalLabel.setMinWidth(120);
+        directCodeLabel.setMinWidth(120);
+        reverseCodeLabel.setMinWidth(120);
+        additionalCodeLabel.setMinWidth(120);
+
+        directCodeField.setMinWidth(320);
+        reverseCodeField.setMinWidth(320);
+        additionalCodeField.setMinWidth(320);
+
+
+        clearButton.setOnAction((ActionEvent event) -> {
+            directCodeField.clear();
+            reverseCodeField.clear();
+            additionalCodeField.clear();
+        });
+
+        decimalButton.setOnAction((ActionEvent event) -> {
+            String text = decimalField.getText();
+
+            if (text == null || text.trim().isEmpty())
+                return;
+
+            int number;
+            text = text.trim();
+
+            try { number = Integer.parseInt(text); }
+            catch ( NumberFormatException nfe ) { return; }
+
+            directCodeField.setText(IntegerUtil.getFormattedCode(IntegerUtil.getDirectCode(number)));
+            reverseCodeField.setText(IntegerUtil.getFormattedCode(IntegerUtil.getReverseCode(number)));
+            additionalCodeField.setText(IntegerUtil.getFormattedCode(IntegerUtil.getAdditionalCode(number)));
+        });
+
+        //HBox with spacing = 5
+        HBox hbox1 = new HBox(5);
+        HBox hbox2 = new HBox(5);
+        HBox hbox3 = new HBox(5);
+        HBox hbox4 = new HBox(5);
+
+        hbox1.getChildren().addAll(decimalLabel, decimalField, decimalButton);
+        hbox2.getChildren().addAll(directCodeLabel, directCodeField);
+        hbox3.getChildren().addAll(reverseCodeLabel, reverseCodeField);
+        hbox4.getChildren().addAll(additionalCodeLabel, additionalCodeField);
+
+        hbox1.setAlignment(Pos.TOP_LEFT);
+        hbox2.setAlignment(Pos.TOP_LEFT);
+        hbox3.setAlignment(Pos.TOP_LEFT);
+        hbox4.setAlignment(Pos.TOP_LEFT);
+
+        VBox vb = new VBox(10);
+        vb.getChildren().addAll(hbox1, hbox2, hbox3, hbox4, clearButton);
+
+        return vb;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Decimal converter to a direct / reverse / additional code");
+        primaryStage.setScene(new Scene(createContent(), 600, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
